020c2d861a36c71148f5ee98eb8542a9ec29f4b2
Michael Westbom <michael.westbom@alphasights.com>
2014-07-22 10:20:53 -0400

    Did some rearranging, added a basic layout

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-var koala = require('koala')
-  , app = koala()
+var koa = require('koa')
+  , router = require('koa-router')
+  , hbs = require('koa-hbs')
+  , path = require('path')
 
+var app = koa()
 module.exports = app
 
-app.use(function *() {
-  this.body = 'Hello World'
+require('./config')(app)
+
+app.use(hbs.middleware({
+  viewPath: path.join(__dirname, 'app', 'views'),
+  defaultLayout: 'layout'
+}))
+
+if (app.env === 'development') {
+  var dotenv = require('dotenv')
+  dotenv.load()
+}
+
+app.keys = [process.env.SESSION_SECRET]
+app.use(router(app))
+
+console.log(app.keys)
+
+app.get('/', function *(next) {
+  yield this.render('index', {issues: []})
 })
