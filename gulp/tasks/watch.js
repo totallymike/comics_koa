475b49c8cf9afddd6697ccf23f39d496bbbf317f
Michael Westbom <michael.westbom@alphasights.com>
2014-07-25 15:09:13 -0400

    WIP build improvements

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,11 +1,28 @@
 var gulp = require('gulp')
+  , debug = require('debug')('gulp')
+  , nodemon = require('gulp-nodemon')
 
 gulp.task('watch', function () {
   var livereload = require('gulp-livereload')
   livereload.listen()
 
-  gulp.watch(global.APP_LINT_PATHS, ['lint:app'])
   gulp.watch('app/assets/javascripts/**', ['lint:client', 'browserify'])
   gulp.watch('app/assets/stylesheets/**', ['less'])
   gulp.watch('public/**').on('change', livereload.changed)
+
+  nodemon({
+    script: './bin/www',
+    watch: ['./app', 'app.js'],
+    ignore: ['./app/assets'],
+    cwd: process.cwd(),
+    env: {'DEBUG': 'comics:*'},
+    nodeArgs: ['--harmony']
+  }).on('change', ['lint:app'])
+    .on('restart', function () {
+      debug('Restarted app')
+    })
+    .on('restart', livereload.changed)
+    .on('message', function (message) {
+      console.log(message)
+    })
 })
